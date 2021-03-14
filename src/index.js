d6dd85e8d0f240096c7cebc1be4d91313ee47c90
Pavel <epiccinemabattles@gmail.com>
2021-03-15 00:13:35 +0300

    feat: add realization

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
+  return !array||array.length<1 ? 0 : array.reduce((prev,curr)=>{
+    return prev<curr ? prev : curr;
+  }, array[0])
 }
 
 exports.max = function max (array) {
-  return 0;
+  return !array||array.length<1 ? 0 : array.reduce((prev,curr)=>{
+    return prev>curr ? prev : curr;
+  }, array[0])
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return !array||array.length<1 ? 0 : array.reduce((prev,curr, index)=>{
+    return index==array.length-1 ? (prev+curr)/array.length : prev+curr;
+  }, 0)
 }
